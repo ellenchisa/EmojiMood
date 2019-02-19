b7f24216ece228a661a4a9caab7d98f8e34c5c9c
Alice Q. Wong <alice@valor.me>
2019-02-18 16:22:35 -0800

    checking styling

--- screens/CheckinScreen.js
+++ screens/CheckinScreen.js
@@ -54,13 +54,21 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{height: 50, width: "100%"}}>{this.state.text}</Text>
-        <Button
-        onPress={this.postMoods}
-        title="Save"
-        />  
+        <View style={styles.top}>
+          <Text style={styles.displayText}>{this.state.text}</Text>
+          <Button
+            style={styles.postButton}
+            onPress={this.postMoods}
+            title="Save"
+          />
+        </View>  
         <View style={{padding: 0, height: 500}}>
         <EmojiInput
+          keyboardBackgroundColor="transparent"
+          enableFrequentlyUsedEmoji={false}
+          categoryFontSize={32}
+          categoryLabelHeight={20}
+          categoryLabelTextStyle={styles.catLabel}
           onEmojiSelected={(emoji) => {
             const all = this.state.text + emoji.char;
             this.setState({text: all})
@@ -70,116 +78,32 @@ export default class HomeScreen extends React.Component {
         </View>
       );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-        </Text>
-        );
-
-      return (
-        <Text style={styles.developmentModeText}>
-        Development mode is enabled, your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-        </Text>
-        );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-        You are not in development mode, your app will run at full speed.
-        </Text>
-        );
-    }
-  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
+  top: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    justifyContent: 'space-between',
+    height: 50
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  displayText: {
+    padding: 2,
+    fontSize: 16
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  postButton: {
+    width: 20,
+    backgroundColor: 'pink'
   },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  catLabel: {
+    fontSize: 12,
+    fontWeight: 'normal',
+    color: '#aaa'
+  }
 });
